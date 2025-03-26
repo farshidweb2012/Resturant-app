0f4ca77b8d82690d0081f62cac99ea2d6f0cd3ef
farshid <40495236+farshidweb2012@users.noreply.github.com>
2025-03-26 11:03:16 +0330

    changes

--- src/Component/fooditem/FoodItem.jsx
+++ src/Component/fooditem/FoodItem.jsx
@@ -1,29 +1,29 @@
-import React, { useState } from 'react'
+
 import './fooditem.css';
 
 
 
 
-const FoodItem = ({id,name,price,image,category}) => {
+const FoodItem = ({ id, name, price, image, category }) => {
 
-  const [itemCount,setItemCount]=useState(0)
 
-   
+
+
   return (
     <div className='fooditem'>
-        <div className='foodtem-image-container'>
-            <img className='fooditem-image'  src={image} alt={name}/>
-            
-        </div>
+      <div className='foodtem-image-container'>
+        <img className='fooditem-image' src={image} alt={name} />
+
+      </div>
+      <div className='fooditem-info'>
         <div className='fooditem-info'>
-         <div className='fooditem-info'>
-         <p className='fooditem-info-name'>{name}</p>
-          
-         </div>
-          
-          <p className='fooditem-info-price'>${price}</p>
-          
+          <p className='fooditem-info-name'>{name}</p>
+
         </div>
+
+        <p className='fooditem-info-price'>${price}</p>
+
+      </div>
     </div>
   )
 }
