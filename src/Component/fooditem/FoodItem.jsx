026c28d912bbb18c2850c6db095d907e07ea0c4e
farshid <40495236+farshidweb2012@users.noreply.github.com>
2025-04-01 00:03:39 +0330

    last

--- src/Component/fooditem/FoodItem.jsx
+++ src/Component/fooditem/FoodItem.jsx
@@ -1,5 +1,9 @@
 
+import { faPlus, faSubtract } from '@fortawesome/free-solid-svg-icons'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './fooditem.css';
+import { useState } from 'react';
 
 
 
@@ -7,24 +11,32 @@ import './fooditem.css';
 
 const FoodItem = ({ id, name, price, image, category }) => {
 
-
-
-
+  const [itemcount, setItemCount] = useState(0)
   return (
     <div className='fooditem'>
       <div className='foodtem-image-container'>
         <img className='fooditem-image' src={image} alt={name} />
       </div>
+
+
       
-     <div className='fooditem-icon'>
-      +
-     </div>
+      <div className='fooditem-icon'>
+        {!itemcount ? <FontAwesomeIcon onClick={() => setItemCount(prev => prev + 1)} icon={faPlus} size='2x' className='icon' /> :
+          <div className='item-counter-icon'>
+            <FontAwesomeIcon className='mines-icon' onClick={() => setItemCount(prev => prev - 1)} icon={faSubtract} size='2x' color='red' />
+            <p style={{ fontSize: '1.5rem' }}>{itemcount}</p>
+            <FontAwesomeIcon className='plus-icon' onClick={() => setItemCount(prev => prev + 1)} icon={faPlus} size='2x' color='green' />
+          </div>}
+      </div>
+
+
+
+
       <div className='fooditem-info'>
         <div className='fooditem-info'>
           <h3 className='fooditem-info-name'>{name}</h3>
         </div>
         <p className='fooditem-info-price'>${price}</p>
-
       </div>
     </div>
   )
--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,11 +1,26 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { foodList } from "../Shop_Asset/Productlist";
 
 export const StoredContext = createContext(null);
 
 const StoredContextProvider = (props) => {
+
+  const [cartItem, setCartItem] = useState({});
+
+  const addToCart = (itemid) => {
+    if (!cartItem[itemid]) {
+      setCartItem((prev) => ({ ...prev, [itemid]: 1 }))
+    }
+    else {
+      setCartItem((prev) => ({...prev,[itemid]:prev[itemid]+1}))
+    }
+  }
+
+
+
   const contextValue = {
     foodList,
+    addToCart
   };
   return (
     <StoredContext.Provider value={contextValue}>
